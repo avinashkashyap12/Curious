53373d704105e5283eb0b2eaf24907b8ad83ce71
Avinash <avinash@headerlabs.com>
2015-01-09 10:46:33 +0530

    Initial Commit

---
+++ ScannerProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScannerProject
+//
+//  Created by Avinash kashyap on 09/01/15.
+//  Copyright (c) 2015 Avinash kashyap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScannerProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScannerProject
+//
+//  Created by Avinash kashyap on 09/01/15.
+//  Copyright (c) 2015 Avinash kashyap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
